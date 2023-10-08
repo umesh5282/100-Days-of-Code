914858217be3b239ac616c442ce632ef26294007
UMESH SHARMA <umesharma5282@gmail.com>
2023-10-08 18:48:40 +0530

    day 16 Insertion sort

---
+++ src/sortingQuestion/InsertionSort.java
@@ -0,0 +1,26 @@
+package sortingQuestion;
+
+public class InsertionSort {
+	
+	public static void printArray(int arr[]) {
+		 for(int i=0; i<arr.length;i++) {
+			 System.out.print(arr[i]+" ");
+		 }
+	}
+          public static void main(String[] args) {
+			int arr[]= {7,8,6,9,2,3};
+			
+			// insrtion sort
+			for(int i=1;i<arr.length;i++) {
+				int current =arr[i];
+				int j=i-1;
+				while(j>=0&& current<arr[j]) {
+					arr[j+1]=arr[j];
+					j--;
+				}
+				
+				arr[j+1]=current;
+			}
+			printArray(arr);
+		}
+}
